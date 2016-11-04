613a8c962fca9a84f70c57bdfb3fecf33b6ad97d
Jacob Friis Heidelbach <jacob@Jacobs-MacBook-Pro.local>
2016-11-04 23:10:57 +0100

    Fixed region typo

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=ueu-central-1
+export AWS_DEFAULT_REGION=eu-central-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=ecs1.1pdevelopment.com
 
